c8a4a32a4f294b9ab6a28a0c8cd4ad6134f05ad5
Adiziel <aditya.pathak.plus@gmail.com>
2020-08-02 16:45:50 +0530

    Minor design changes

--- steer/settings.py
+++ steer/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from .secret_settings import SECRET_KEY
+from .secret_settings import SECRET_KEY,  SECRET_SMTP_PASSWORD, SECRET_SMTP_EMAIL
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -144,5 +144,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD=''
+EMAIL_HOST_USER = SECRET_SMTP_EMAIL
+EMAIL_HOST_PASSWORD = SECRET_SMTP_PASSWORD
